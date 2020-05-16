cf1a6653603ca845379c25fa3616a37375e827ca
Alain Picard <Dr.Alain.Picard@gmail.com>
2020-05-16 11:24:41 +1000

    Minor tweaks.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject hello-world.core "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+  :description "Playground in which to learn some clojurescript and re-frame."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -19,5 +19,5 @@
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.3"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
-                   }})
 
+                   :repl-options { :timeout 120000}}})
--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -3,21 +3,33 @@
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]))
 
+
+;; This command will cause our printlns to also show up in the console's log,
+;; which can sometimes be useful.
+(enable-console-print!)
+
+
 (println "This text is printed from src/hello_world/core.cljs. Go ahead and edit it and see reloading in action.")
+
 
 (defn multiply [a b] (* a b))
 
 
+
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello class!"}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
 (defn hello-world []
   [:div
+
    [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/hello_world/core.cljs and watch it change!"]])
+   [:h3 "To infinity, and beyond! "(/ 1 0)]
+   [:h3 "We're not in Kansas anymore: (+ 1 nil) " (+ 1 nil) " should be a type error..."]
+   [:p "Go watch this talk: " [:a {:href  "https://www.destroyallsoftware.com/talks/wat"} "Wat!?"]]
+   [:h3 "Edit this in src/hello_world/core.cljs and WATCH IT CHANGE!"]])
 
 (defn mount [el]
   (reagent/render-component [hello-world] el))
