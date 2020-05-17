04f51e7ee20cc7913c7a9bcc7e605b5de0e82e8b
Alain Picard <Dr.Alain.Picard@gmail.com>
2020-05-17 15:13:49 +1000

    Form-2 components

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -9,11 +9,64 @@
 ;; which can sometimes be useful.
 (enable-console-print!)
 
-;;; Integrating Reagent with core.async.
+;;; A bit more Reagent: form-1 vs form-2 components
+;;
+;;  Good doc at: https://cljdoc.org/d/reagent/reagent/1.0.0-alpha2/doc/documentation-index
 
 
+;; Sometimes our components want to maintain their own local state.
+
+;; Consider this one:
+(defn timer-component [label update-frequency]
+  (let [seconds-elapsed (reagent/atom 0)] ;; setup, and local state
+    (fn [label update-frequency]          ;; inner, render function is returned
+      (go
+        (<! (timeout (* 1000 update-frequency)))
+        (swap! seconds-elapsed + update-frequency))
+      [:div
+       "Component: " label
+       ": seconds Elapsed: " @seconds-elapsed])))
+
+;; Each new component, displayed in [timer-component] will have their own
+;; local atom, and local argument to the function, independent of each other.
+
+;;;; Cursors
+
+;; First create a ratom
+(def state (reagent/atom {:foo {:bar "BAR"}
+                                :baz "BAZ"
+                                :quux "QUUX"}))
+;; Now create a cursor
+(def bar-cursor (reagent/cursor state [:foo :bar]))
+
+(defn state-component []
+  (println "Redisplaying STATE")
+  [:div "State: " @state])
+
+(defn quux-component []
+  (println "Redisplaying QUUX")
+  [:div (:quux @state)])
+
+(defn bar-component []
+  (println "Redisplaying BAR")
+  [:div @bar-cursor])
+
+
+;; Now if only a partial part of the state mutates: (watch the console log)
+(comment
+  (swap! state assoc-in [:foo :bar] "The new bar")
+
+  (swap! state assoc :baz "A new key appears"))
+
+
+;; This idea will be used (implicitly) by the re-frame framework
+;; which we'll study next class.
+
+
+
+
 ;; Let us declare our state globally:
-(defonce the-counter (atom 0))
+(defonce the-counter (reagent/atom 0))
 
 ;; Now let us create a communication channel which our
 ;; UI can sent event to.
@@ -47,7 +100,12 @@
 (defn simple-button []
   [:div
    [:center
-    [:h1 "Simple button example"]
+    [:h1 "Timer component"]
+    [timer-component "Fast" 1]
+    [timer-component "Slow" 3]
+    [state-component]
+    [quux-component]
+    [bar-component]
     [:input {:type :button :class :button :value "Push me!"
              :on-click #(dispatch-event! {:type :increment})}]
     [:div#the-text @the-counter]]])
